782387c17a5fc396f0212f0a7dfc0f1db1c2ea91
杜晓星 <1255322700@qq.com>
2020-03-11 10:45:53 +0800

    文档变更

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="sentry-dingding",
-    version='0.0.4',
-    author='ansheng',
-    author_email='ianshengme@gmail.com',
-    url='https://github.com/anshengme/sentry-dingding',
+    name="sentry-dingding2",
+    version='0.0.1',
+    author='Sentrydingding2',
+    author_email='1255322700@qq.com',
+    url='https://github.com/y1024/sentry-dingding',
     description='A Sentry extension which send errors stats to DingDing',
     long_description=long_description,
     long_description_content_type="text/markdown",
